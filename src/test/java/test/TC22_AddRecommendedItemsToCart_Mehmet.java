2c32ddfd581ea3eac153b99ceefbde4d142b39cb
mehmetvural <mehmetvurall@gmail.com>
2022-02-01 20:43:31 -0500

    I started to code...

--- src/test/java/test/TC22_AddRecommendedItemsToCart_Mehmet.java
+++ src/test/java/test/TC22_AddRecommendedItemsToCart_Mehmet.java
@@ -1,5 +1,19 @@
 package test;
 
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+import java.util.concurrent.TimeUnit;
+
 public class TC22_AddRecommendedItemsToCart_Mehmet {
 
+    @Test
+    public void VerifyAllProducts(){
+        Driver.getDriver().get(ConfigReader.getProperty("app_url"));
+        Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        
+    }
 }
