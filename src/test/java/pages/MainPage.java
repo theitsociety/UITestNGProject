e849d42c95064e60efbbeae65a65073d9a581c8f
Orcun FAZLI <30703148+ofazli@users.noreply.github.com>
2022-02-03 22:17:29 -0500

    Update MainPage.java

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -39,5 +39,16 @@ public class MainPage {
 
     @FindBy(xpath = "//a[@href='/view_cart']")
     public WebElement cartButton;
+    
+      @FindBy(xpath = "//a[text()=' Signup / Login']")
+    public WebElement signupLogin;
+    @FindBy(xpath = "//a[text()=' Logged in as ']")
+    public WebElement loggedInAs;
+    @FindBy(xpath = "//a[text()=' Delete Account']")
+    public WebElement delete_Account;
+    @FindBy(partialLinkText = "Test Cas")
+    public WebElement test_case1;
+    @FindBy(xpath = "//img[@alt='Website for automation practice']")
+    public WebElement isDispley;
 
 }
