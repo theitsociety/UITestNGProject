71a7694ced10ac43bb001edeffe023d4fbd39bd8
~!@#$%^&*()_+}{ <88084445+emrahselek@users.noreply.github.com>
2022-02-05 00:53:07 -0500

    es

--- src/test/java/test/TC02_LoginUserWithCorrectData_Emrah.java
+++ src/test/java/test/TC02_LoginUserWithCorrectData_Emrah.java
@@ -16,12 +16,42 @@ public class TC02_LoginUserWithCorrectData_Emrah {
 
     @Test
     public void TC02(){
+//        1. Launch browser
+//        2. Navigate to url 'http://automationexercise.com'
         Driver.getDriver().navigate().to("https://automationexercise.com");
-        //Driver.getDriver().get(ConfigReader.getProperty("app_url"));
+
+//        3. Verify that home page is visible successfully
         Assert.assertTrue(Driver.getDriver().findElement(By.xpath("//img[@src='/static/images/home/logo.png']")).isDisplayed());
+
+//        4. Click on 'Signup / Login' button
         mainPage.signupLogin.click();
         ReusableMethods.waitFor(2);
+
+//        5. Verify 'Login to your account' is visible
         System.out.println(Driver.getDriver().getCurrentUrl());
+        String exceptedResult="https://automationexercise.com/login";
+        String actualResult=Driver.getDriver().getCurrentUrl();
+        Assert.assertEquals(actualResult,exceptedResult);
+
+//        6. Enter correct email address and password
+        loginPage.email.sendKeys(ConfigReader.getProperty("email_first"));
+        loginPage.password.sendKeys(ConfigReader.getProperty("password"));
+        ReusableMethods.waitFor(2);
+
+//        7. Click 'login' button
+        loginPage.login.click();
+
+//        8. Verify that 'Logged in as username' is visible
+        Assert.assertTrue(Driver.getDriver().findElement(By.xpath("//*[@class=\"fa fa-user\"]")).isDisplayed());
+
+//        9. Click 'Delete Account' button
+        loginPage.deleteAccount.click();
+
+//        10. Verify that 'ACCOUNT DELETED!' is visible
+        //Note: Account is not deleted. All buttons do not work anymore.
+
+
+
 
 
 
