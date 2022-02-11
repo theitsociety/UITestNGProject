6902e4a6aa4156812b9de03d49f331235ed40f44
Orcun FAZLI <30703148+ofazli@users.noreply.github.com>
2022-02-10 21:51:25 -0500

    Update TC17_Remove_Products_Cart_Orcun.java

--- src/test/java/test/TC17_Remove_Products_Cart_Orcun.java
+++ src/test/java/test/TC17_Remove_Products_Cart_Orcun.java
@@ -1,4 +1,28 @@
 package test;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.MainPage;
+import pages.ViewCartPage;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
 public class TC17_Remove_Products_Cart_Orcun {
+        MainPage mainPage = new MainPage();
+        ViewCartPage viewCartPage = new ViewCartPage();
+
+    @Test
+    public void TC17(){
+        MainPage.setUp();
+        Assert.assertTrue(Driver.getDriver().findElement(By.xpath("//img[@src=\"/static/images/home/logo.png\"]")).isDisplayed());
+        mainPage.frozenTopsForKids.click();
+        mainPage.viewCart.click();
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("Checkout"));
+        viewCartPage.deleteProduct.click();
+        ReusableMethods.waitFor(1);
+        Assert.assertTrue(Driver.getDriver().findElement(By.xpath("//b[text()='Cart is empty!']")).isDisplayed());
+        Driver.closeDriver();
+    }
 }
+
